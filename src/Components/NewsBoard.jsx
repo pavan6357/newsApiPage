2e58e624e1772bb637220dbd7a5ddd075d52bb07
pavan6357 <149621814+pavan6357@users.noreply.github.com>
2024-02-20 21:04:24 +0530

    Update NewsBoard.jsx

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -6,7 +6,7 @@ const NewsBoard = ({category}) => {
     const [articles,setArticles] = useState([]);
 
     useEffect(() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey={}`;
         fetch(url).then(response=> response.json()).then(data=>setArticles(data.articles));
     },[category])
   return (
